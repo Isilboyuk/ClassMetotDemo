4ecb8e86b42cd8cd9e30120720bf76519c8b2157
Isilboyuk <e201593@metu.edu.tr>
2021-01-17 22:29:12 +0300

    Initial commit

---
+++ ClassMetoDemo/MusteriManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetoDemo
+{
+    class MusteriManager
+    {
+        //Add Customer
+        public void AddMusteri(Musteri musteri) {
+            Console.WriteLine(musteri.Name + " " + musteri.Surname + " kişisi başarılı bir şekilde eklenmiştir.");
+        }
+        //Delete Customer
+        public void DeleteMusteri(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Id + " Id' ye sahip " + musteri.Name + musteri.Surname + " başarılı bir şekilde silinmiştir.");
+        }
+        //List Customer
+        public void ListMusteri(Musteri[] musteri)
+        {
+            foreach(Musteri cust in musteri)
+            {
+                Console.WriteLine("Id: " + cust.Id + " Name: " + cust.Name + " Surname: " + cust.Surname);
+            }
+        }
+    }
+}
---
+++ ClassMetoDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ClassMetoDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri m1 = new Musteri();
+            m1.Name = "Ali";
+            m1.Surname = "Can";
+            m1.Id = 1;
+            Musteri m2 = new Musteri();
+            m2.Name = "Veli";
+            m2.Surname = "Can";
+            m2.Id = 2;
+
+            Musteri[] CustomerArray = new Musteri[] { m1,m2 };
+            MusteriManager custManager = new MusteriManager();
+            custManager.AddMusteri(m1);
+            custManager.DeleteMusteri(m1);
+            custManager.ListMusteri(CustomerArray);
+        }
+    }
+}
